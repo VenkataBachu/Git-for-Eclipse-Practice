29628fba337e3739e4742f6720c62292e8880c7e
Venkata Bachu <venkata.bachu@gmail.com>
2023-05-29 12:56:09 -0400

    Added new filed - address and its getters and setters methods

--- src/com/Venkata/Test/Office.java
+++ src/com/Venkata/Test/Office.java
@@ -2,4 +2,15 @@ package com.Venkata.Test;
 
 public class Office {
 
+	private String address;
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 }
