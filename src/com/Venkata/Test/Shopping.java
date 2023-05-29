f8bf3634a738f594f8825484cdf69e83fa4a7670
Venkata Bachu <venkata.bachu@gmail.com>
2023-05-29 13:24:00 -0400

    Added Getters and Setters for Walmart

--- src/com/Venkata/Test/Shopping.java
+++ src/com/Venkata/Test/Shopping.java
@@ -4,4 +4,19 @@ public class Shopping {
 	
 	private String walmart;
 
+	/**
+	 * @return the walmart
+	 */
+	public String getWalmart() {
+		return walmart;
+	}
+
+	/**
+	 * @param walmart the walmart to set
+	 */
+	public void setWalmart(String walmart) {
+		this.walmart = walmart;
+	}
+
+		
 }
