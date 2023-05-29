cc867429e1297e25d090f8648fa58823032fd115
Venkata Bachu <venkata.bachu@gmail.com>
2023-05-29 12:48:13 -0400

    Added new filed - Name  and its setters and getters methods

--- src/com/Venkata/Test/User.java
+++ src/com/Venkata/Test/User.java
@@ -1,5 +1,18 @@
 package com.Venkata.Test;
 
 public class User {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
 
 }
